878650a018625b74bd9e5e77ce1a4a2599d49513
MJWebsites777 <mjjwebsites@gmail.com>
2015-08-25 22:25:09 -0500

    added JS file for variables

---
+++ app/scripts/variables.js
@@ -0,0 +1,13 @@
+//-------------Constants-------------
+var HOME_PAGE = 0;
+var PORTFOLIO_PAGE = 1;
+var ABOUT_PAGE = 2;
+var CONTACT_PAGE = 3;
+//===================================
+
+var winFocus, siteLoading = true;
+var isCtrlDown, disableGoto, pageChanging = false;
+var keyCode, currentProj = null;
+var pageCount = $('.page').length;
+var prevPage = -1;
+var currentPage = 0;
